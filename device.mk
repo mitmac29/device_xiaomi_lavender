e00e53dd0a259ef20a78bf25c4f45d5989413d30
LuK1337 <priv.luk@gmail.com>
2024-11-02 12:45:07 +0100

    lavender: Build libhidlbase-v32{,.vendor}
    Change-Id: I1b31199ec389ea6ba0d8faea4c539839c1cb8170

--- device.mk
+++ device.mk
@@ -198,7 +198,9 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
-    libhwbinder.vendor
+    libhwbinder.vendor \
+    libhidlbase-v32 \
+    libhidlbase-v32.vendor
 
 # IDC
 PRODUCT_COPY_FILES += \
