5b52be705dc7622228965279e6d86ab6826d5471
Crave <aosp@crave.io>
2025-02-21 16:38:24 +0000

    lavender: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# clone device tree
-rm -rf device/xiaomi/lavender;
-git clone https://github.com/Aeoniixx-Lavender/device_xiaomi_lavender -b Flare-15 device/xiaomi/lavender --depth=1;
-
 # clone vendor tree
 rm -rf vendor/xiaomi/lavender;
-git clone https://github.com/Aeoniixx-Lavender/vendor_xiaomi_lavender -b Flare-15 vendor/xiaomi/lavender --depth=1;
+git clone https://github.com/mitmac29/vendor_xiaomi_lavender -b mistify vendor/xiaomi/lavender --depth=1;
 
 # clone kernel tree
 rm -rf kernel/xiaomi/lavender;
-git clone https://github.com/Aeoniixx-Lavender/kernel_xiaomi_lavender-4.19 -b 15.0 kernel/xiaomi/lavender --depth=1;
+git clone https://github.com/mitmac29/kernel_xiaomi_lavender-4.19 -b mistify kernel/xiaomi/lavender --depth=1;
 
 # clone hals
 rm -rf hardware/qcom-caf/sdm660/media;
 rm -rf hardware/qcom-caf/sdm660/audio;
 rm -rf hardware/qcom-caf/sdm660/display;
 
-git clone https://github.com/Aeoniixx-Lavender/hardware_qcom-caf_sdm660_media hardware/qcom-caf/sdm660/media --depth=1;
-git clone https://github.com/Aeoniixx-Lavender/hardware_qcom-caf_sdm660_audio hardware/qcom-caf/sdm660/audio --depth=1;
-git clone https://github.com/Aeoniixx-Lavender/hardware_qcom-caf_sdm660_display hardware/qcom-caf/sdm660/display --depth=1;
+git clone https://github.com/mitmac29/hardware_qcom-caf_sdm660_media hardware/qcom-caf/sdm660/media --depth=1;
+git clone https://github.com/mitmac29/hardware_qcom-caf_sdm660_audio hardware/qcom-caf/sdm660/audio --depth=1;
+git clone https://github.com/mitmac29/hardware_qcom-caf_sdm660_display hardware/qcom-caf/sdm660/display --depth=1;
 
 # Dolby Atmos
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX --depth=1;
+git clone https://github.com/mitmac29/packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX --depth=1;
